7dfdc19fcf2f6b3146989affa922b8b246ad7e91
Sudeep <sudeepstha.96@gmail.com>
2024-03-11 21:05:19 +0545

    feat: getUser feature added.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,10 +52,34 @@ export const signIn = async (req, res, next) => {
     if (!validatePassword) {
       return res.status(403).json({ message: "Invalid password" });
     }
+    const { password: pass, ...rest } = validateEmail._doc;
+
     return res.status(200).json({
       message: "User logged in successfully",
+      rest,
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    // Query the database to find the user by their userId
+    const user = await User.findById(req.params.id);
+
+    // If user is not found, return an error response
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const { password, ...rest } = user._doc;
+
+    // If user is found, return the user details
+    return res.status(200).json(rest);
+  } catch (error) {
+    // If an error occurs, return an error response
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
